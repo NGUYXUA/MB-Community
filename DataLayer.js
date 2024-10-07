4dc7a41d6b28611c7541431556eeccbd4448a227
NGUYXUA <142308096+NGUYXUA@users.noreply.github.com>
2024-10-07 16:20:06 +0200

    Update DataLayer.js

--- DataLayer.js
+++ DataLayer.js
@@ -2,11 +2,11 @@ window.dataLayer_dag = window.dataLayer_dag || [];
 window.dataLayer_dag.push({
     event: "<event>", // "pageView", "userLogin", "itemPurchased"
     site: {
-        language: "<language>", // "de" für Deutsch, "en" für Englisch
+        language: "<language>" // "de" für Deutsch, "en" für Englisch
     },
     user: {
         internal: "<internalTraffic>", // true für internen Traffic, false für externen Traffic
-        loginState: "<loginState>", // "loggedIn", "loggedOut"
+        loginState: "<loginState>" // "loggedIn", "loggedOut"
     },
     page: {
         name: "<pageName>", // "Homepage”
